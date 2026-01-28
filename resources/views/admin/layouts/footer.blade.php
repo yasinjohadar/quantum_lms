7f3bc82ee564de0427576b4974e42fc699588276
yasin <yassinjokhdar@gmail.com>
2026-01-28 13:56:50 +0300

    add frontend dashboard12545

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,13 +1,11 @@
     <!-- Footer Start -->
         <footer class="footer mt-auto py-3 bg-white text-center">
             <div class="container">
-                <span class="text-muted"> Copyright © <span id="year"></span> <a
-                        href="javascript:void(0);" class="text-dark fw-semibold">Valex</a>.
-                    Designed with <span class="bi bi-heart-fill text-danger"></span> by <a href="javascript:void(0);">
-                        <span class="fw-semibold text-primary text-decoration-underline">Spruko</span>
-                    </a> All
-                    rights
-                    reserved
+                <span class="text-muted"> حقوق النشر © <span id="year"></span> <a
+                        href="https://claudsoft.com" target="_blank" rel="noopener noreferrer" class="text-dark fw-semibold">كلاوسوفت</a>.
+                    صُمّم بـ <span class="bi bi-heart-fill text-danger"></span> بواسطة <a href="https://claudsoft.com" target="_blank" rel="noopener noreferrer">
+                        <span class="fw-semibold text-primary text-decoration-underline">كلاوسوفت</span>
+                    </a>. جميع الحقوق محفوظة
                 </span>
             </div>
         </footer>
