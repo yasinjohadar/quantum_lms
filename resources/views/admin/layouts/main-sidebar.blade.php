8e7a6477f53dd36118d76a145b28c4ce6c4474f2
Yassin jokhadar <yasin@gmail.com>
2026-03-31 01:06:08 -0800

    add new code for rols for subervisor4

--- resources/views/admin/layouts/main-sidebar.blade.php
+++ resources/views/admin/layouts/main-sidebar.blade.php
@@ -196,7 +196,7 @@
                                 <li class="slide {{ request()->is('admin/academic-years*') || request()->is('admin/academic-weeks*') ? 'active' : '' }}">
                                     <a href="{{ route('admin.academic-years.index') }}" class="side-menu__item {{ request()->is('admin/academic-years*') ? 'active' : '' }}">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" style="width: 18px; height: 18px;"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM9 10H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2z"/></svg>
-                                        <span class="side-menu__label">السنوات والأسابيع الدراسية</span>
+                                        <span class="side-menu__label">السنوات الدراسية</span>
                                     </a>
                                 </li>
                                 @endcanany
